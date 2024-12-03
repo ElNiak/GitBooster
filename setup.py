143993d90822e65d631e62e55ba267d1d9109389
ElNiak <christophe.crochet@uclouvain.be>
2024-12-03 10:33:18 +0100

    update setup with long description and also add dummy test

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 # PANTHER-SCP/panther/setup.py
 
+import codecs
+import os
 from setuptools import setup, find_packages
 
+
+# Get the long description from the README file
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+  with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+      long_description = f.read()
+except:
+  # This happens when running tests
+  long_description = None
 setup(
     name='GitBooster',
     version='0.0.1',
@@ -19,6 +30,7 @@ setup(
     author='ElNiak',
     author_email='your.email@example.com',
     description='GitBooster:  Boosts your GitHub stats with automated, scheduled commits to keep your profile active.',
+    long_description=''
     url='https://github.com/ElNiak/GitBooster',
     classifiers=[
         'Programming Language :: Python :: 3',
